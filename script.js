3148f7c8eeb919f116d055bb510c3f724dc5e3bf
pavithiran <pavithransaravanan22@gmail.com>
2023-06-03 15:00:40 +0530

    Add feedback for required fields

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
 const pass = document.querySelector("#pass");
 const pass2 = document.querySelector("#pass2");
+const fname = document.querySelector("#fname");
+const email = document.querySelector("#email");
+const submit = document.querySelector('.submit');
 
-
-
-pass2.addEventListener('input', (e)=>{
-    if(!pass2.value){
-        pass2.setAttribute("isValid", "valid");
-        pass.setAttribute("isValid", "valid");
+submit.addEventListener('click', (e)=>{
+    fname.required = true;
+    email.required = true;
+    pass.required = true;
+    pass2.required = true;
+    if(fname.checkValidity() && email.checkValidity() && pass.checkValidity() && pass2.checkValidity() && pass.value == pass2.value){
         return;
     }
+    else e.preventDefault();
+})
+
+fname.addEventListener('focus', (e)=>{
+    fname.required = true;
+})
+email.addEventListener('focus', (e)=>{
+    email.required = true;
+})
+pass.addEventListener('focus', (e)=>{
+    pass.required = true;
+})
+pass2.addEventListener('focus', (e)=>{
+    pass2.required = true;
+})
+
+pass2.addEventListener('input', (e)=>{
+    if(!pass2.value) return;
     if(pass2.value != pass.value){
-        console.log("passwords do not match");
         pass2.setAttribute("isValid", "invalid");
         pass.setAttribute("isValid", "invalid");
     }
     else{
-        console.log("match");
         pass2.setAttribute("isValid", "valid");
         pass.setAttribute("isValid", "valid");
     }
 })
 
 pass.addEventListener('input', (e)=>{
-    if(!pass2.value){
+    if(!pass.value && !pass2.value){
         pass2.setAttribute("isValid", "valid");
         pass.setAttribute("isValid", "valid");
         return;
     }
+    if(!pass2.value) return;
     if(pass2.value != pass.value){
         console.log("passwords do not match");
         pass2.setAttribute("isValid", "invalid");
         pass.setAttribute("isValid", "invalid");
     }
     else{
-        console.log("match");
         pass2.setAttribute("isValid", "valid");
         pass.setAttribute("isValid", "valid");
     }
